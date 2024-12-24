f080611ff3e93bdad7333d88eefb23d47b6dbe5b
EngrArfin <arfin.cse.green.edu.bd@gmail.com>
2024-12-25 03:12:39 +0600

    kfc setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <div className="hero-section">
+        <h1 className="title">Welcome to KFC King Food Cafe</h1>
+      </div>
+
+      <div className="cta-section">
+        <button className="cta-button">Order Now</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
